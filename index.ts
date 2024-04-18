63f862077b4ce650543eff1c59efa8c668dad51c
gastonfalena <gastonfalena@gmail.com>
2024-04-18 18:25:47 -0300

    creating ts

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+// Configuration the .env file
+
+dotenv.config();
+
+//Create Express APP
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+
+// Define the first Route of APP
+app.get("/", (req: Request, res: Response) => {
+  res.send("APP Express + TS");
+});
+
+// Execute APP and Listen Ruequest to PORT
+app.listen(port, () =>
+  console.log(`EXPRESS SERVER: Running at http://localhost:8000`)
+);
